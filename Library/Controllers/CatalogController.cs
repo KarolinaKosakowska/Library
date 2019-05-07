9de4a333b16363a5aead008f4aead8a772a589a7
karolina.kosakowska.email@gmail.com <karolina.kosakowska.email@gmail.com>
2019-05-07 12:28:23 +0200

    View models

---
+++ Library/Controllers/CatalogController.cs
@@ -0,0 +1,35 @@
+﻿using Library.Models.Catalog;
+using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Library.Controllers
+{
+    public class CatalogController: Controller
+    {
+        private readonly ILibraryAsset assets;
+
+        public CatalogController(ILibraryAsset assets)
+        {
+            this.assets = assets;
+        }
+        public IActionResult Index()
+        {
+            var assetModels = assets.GetAll();
+            var listingResult = assetModels
+                .Select(result => new AssetIndexListingModel
+                {
+                    Id = result.Id,
+                    ImageUrl=result.ImageUrl,
+                    AuthorOrDirector=assets.GetAuthorOrDirector(result.Id),
+                    DeweyCallNumber=assets.GetDeweyIndex(result.Id),
+                    Title=result.Title,
+                    Type=assets.GetType(result.Id)
+
+
+
+
+            } );
+        }
+    }
+}
